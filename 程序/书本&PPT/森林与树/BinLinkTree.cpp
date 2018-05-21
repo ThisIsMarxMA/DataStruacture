7eabbd7ef31efa0243a1abbc752e326a40f6e6df
ThisIsMarxMA <ma376371893@163.com>
2018-05-21 21:34:55 +0800

    树与森林修改

--- 程序/书本&PPT/森林与树/BinLinkTree.cpp
+++ 程序/书本&PPT/森林与树/BinLinkTree.cpp
@@ -1,12 +1,14 @@
 typedef char TreeData;
 
 //二叉链表表示二叉树
+//单个节点定义
 typedef struct node
 {
 	TreeData data;
 	struct node *leftChild,*rightChild;	
 }BinTreeNode;
 typedef BinTreeNode *BinTree;
+
 
 //自下而上依次删除树
 void destory(BinTreeNode *current)
@@ -75,6 +77,56 @@ void PostOrder(BinTreeNode *T)
 	}
 }
 
+void CreateBiTree(BinTree &T,char s[],int &i)  //此处i的初值为-1
+{
+	i++;
+	if(s[i]==' '||s[i]=='#')
+		T=NULL;
+	else
+	{
+		T=new BinTreeNode;
+		T->data=s[i];
+		CreateBiTree(T->leftChild,s,i);
+		CreateBiTree(T->rightChild,s,i);
+	}
+}
 
+int Count(BinTreeNode *T)
+{
+	if(T==NULL)
+		return 0;
+	else
+		return 1+Count(T->leftChild)+Count(T->rightChild);
+}
 
+int Leaf_Count(BinTree T)
+{
+	if(!T)
+		return 0;
+	else if(!T->leftChild&&!T->rightChild) return 1;
+	else return Leaf_Count(T->leftChild)+Leaf_Count(T->rightChild);
+}
+
+int Height(BinTreeNode *T)
+{
+	if(T==NULL)
+		return 0;
+	else
+	{
+		int m=Height(T->leftChild);
+		int n=Height(T->rightChild);
+		return (m>n)?m+1:n+1;
+	}
+}
+
+BinTree Copy(BinTree T)
+{
+	if(T==NULL)
+		return 0;
+	BinTree temp=new BinTreeNode;
+	temp->data=T->data;
+	temp->leftChild=Copy(T->leftChild);
+	temp->rightChild=Copy(T->rightChild);
+	return temp;
+}
 
